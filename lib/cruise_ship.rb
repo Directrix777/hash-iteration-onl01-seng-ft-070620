3f74939ae3ea372239d4ac384afcb22c35e9e685
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-09 16:56:02 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,9 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
   passengers.each do |suite, name|
-    if suite == :suite_a && name.chr == "a"
-      winner = name
+    if suite == :suite_a && name.chr == "A"
+      return name
     end
   end
-  winner
 end
